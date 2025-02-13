647bb7f16cf6e83b45ddc8e5a5367f2510c82417
Logan Scholz <80925426+LoganScholz-PING@users.noreply.github.com>
2025-02-13 15:18:52 -0700

    omg

---
+++ TemposonicSSICommunication/src/TemposonicSSICommunication.h
@@ -0,0 +1,28 @@
+#ifndef TEMPOSONICSSICOMMUNICATION_H
+#define TEMPOSONICSSICOMMUNICATION_H
+
+/*
+
+There are two wiring strategies:
+1. Ground CLOCK- and DATA-. Then wire CLOCK+ and DATA+ into the Arduino Mega's digital input pins
+2. Use a Differential to single-ended converter (like the MAX485) to convert the differential signals to single-ended signals
+
+Library written specifically for the Temposonics LVDT part # EL00300UD841S2B5100
+
+*/
+
+class TempoSSI
+{
+    public: 
+        TempoSSI(int DATA_PIN, int CLOCK_PIN, int BIT_COUNT);
+
+        unsigned long ReadPosition();
+        unsigned long ReadPositionNoMask();
+    
+    private:
+        int DATA_PIN;
+        int CLOCK_PIN;
+        int BIT_COUNT;
+};
+
+#endif
